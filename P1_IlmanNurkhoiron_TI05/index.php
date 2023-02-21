af9b70957708532b8075ea570129c2512a9791ce
IlmanNK <ilmanponka345@gmail.com>
2023-02-21 15:02:11 +0700

    Pertemuan Pertama

---
+++ P1_IlmanNurkhoiron_TI05/index.php
@@ -0,0 +1,12 @@
+<?php 
+    $nama = 'Ilman Nurkhoiron';
+    $umur = 20;
+    $berat = 75.2;
+
+    echo 'Nama saya :'. $nama;
+    echo '</br>Umur saya :'. $umur;
+    echo '</br>berat saya :'.$berat;
+
+
+
+?>
---
+++ P1_IlmanNurkhoiron_TI05/tes2.php
@@ -0,0 +1,9 @@
+<?php
+    $ar_buah = ['Pepaya', 'Mangga', 'Pisang', 'Jambu'];
+
+    foreach( $ar_buah as $buah) {
+        echo $buah;
+    }
+
+
+?>
